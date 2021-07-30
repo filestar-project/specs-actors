e34b336da4177165e34dcc6d2494805f630389f7
codefather <codefather@filestar.net>
2021-07-30 17:32:19 +0800

    update vm.deleteActor

--- support/vm/vm.go
+++ support/vm/vm.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	hamt "github.com/filecoin-project/go-hamt-ipld/v2"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/cbor"
@@ -238,11 +237,8 @@ func (vm *VM) SetActorState(ctx context.Context, key address.Address, state cbor
 // This behaviour is based on a principle that some store implementations might not be able to determine
 // whether something exists before deleting it.
 func (vm *VM) deleteActor(_ context.Context, key address.Address) error {
-	err := vm.actors.Delete(abi.AddrKey(key))
-	vm.actorsDirty = true
-	if err == hamt.ErrNotFound {
-		return nil
-	}
+	found, err := vm.actors.TryDelete(abi.AddrKey(key))
+	vm.actorsDirty = found
 	return err
 }
 
